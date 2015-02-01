48ca61626d157b2d0e7a3ef074b62026a4656260
Pouya Arvandian <pouya314@gmail.com>
2015-02-01 15:25:42 +0800

    Initial commit.
    kickstart development of coding test with a project skeleton.

---
+++ lib/redbubble/errors.rb
@@ -0,0 +1,6 @@
+class RedBubbleException < Exception; end
+
+class WrongNoOfArgs < RedBubbleException; end
+class InputFileDoesNotExist < RedBubbleException; end
+
+# ... etc
---
+++ lib/redbubble/runner.rb
@@ -0,0 +1,8 @@
+require_relative 'rbsystem_controller'
+
+module Redbubble
+  
+  # This class takes input from user, routes the input to controller, and returns the response eventually.
+  class Runner
+  end
+end
