52ba43e62d07e38393df35e1bc070003bad8ba7d
Pouya Arvandian <pouya314@gmail.com>
2015-02-02 22:46:27 +0800

    Implement HTML Generation.
    Produce a single HTML file for each camera make, camera model and also an index.
    
    The index HTML page must contain:
    - Thumbnail images for the first 10 work
    - Navigation that allows the user to browse to all camera makes
    
    Each Camera Make HTML page must contain:
    - Thumbnail images of the first 10 works for that camera make
    - Navigation that allows the user to browse to the index page and to all camera models of that make
    
    Each Camera Model HTML page must contain:
    - Thumbnail images of all works for that camera make and model
    - Navigation that allows the user to browse to the index page and the camera make
    
    [Note]
    I chose to sanitise the works collection by removing those work objects that lack either: thumbnail url /or/ Make name /or/ Model name. This decision was made based on common sense, because if you think about it, it wouldn't make sense to (for instance) display a work on the index page that is missing a model - user will keep drilling down model links to find out which model was used to take that particular picture, but won't be able to find the picture in any of the model pages. Of course I understand that, in a real world situation every functional detail must be clarified with the product owner/client, but due to our long question-response cycle time, I took the liberty to make the above assumption. :)  I hope it is alright with you.

--- lib/redbubble/html_generation.rb
+++ lib/redbubble/html_generation.rb
@@ -1,27 +1,71 @@
+require 'erubis'
+require_relative 'settings'
+
 module Redbubble
   class HtmlGeneration
+    attr_accessor :works, :output_dir_path, :output_template
+
     def initialize(works, output_dir_path)
-      @works = works
+      @works = works.delete_if { |work| work.invalid? }  # sanitization of works happens here!
       @output_dir_path = output_dir_path
+      @output_template = Erubis::Eruby.new(File.read(OUTPUT_TEMPLATE))
     end
-    
-    def go
-      produce_index!
-      produce_makes!
-      produce_models!
+
+    def go!
+      # start with index
+      produce_index_page(works)
     end
-    
+
     private
-      def produce_index!
-        
+      def produce_index_page(works)
+        makes = works.group_by { |work| work.make_name}
+
+        create_html_file(
+          INDEX[:file_name],
+          INDEX[:title],
+          makes.keys,
+          works.take(10).map { |work| work.url }
+        )
+
+        # next: pages for makes
+        produce_makes_pages(makes)
       end
-      
-      def produce_makes!
-        
+
+      def produce_makes_pages(makes)
+        makes.each do |make, items|
+          models = items.group_by { |item| item.model_name }
+
+          create_html_file(
+            "#{make}.html",
+            "#{make}",
+            models.keys.unshift(INDEX[:title]),
+            items.take(10).map { |i| i.url }
+          )
+
+          # finally: pages for models
+          produce_models_pages(models, make)
+        end
       end
-      
-      def produce_models!
-        
+
+      def produce_models_pages(models, make_backref)
+        models.each do |model, items|
+          create_html_file(
+            "#{model}.html",
+            "#{model}",
+            [INDEX[:title], make_backref],
+            items.map { |i| i.url }    # show all here, not the first 10
+          )
+        end
+      end
+
+      def create_html_file(filename, title, nav, thumbnails)
+        File.open("#{output_dir_path}/#{filename}", "w") do |f|
+          f.puts output_template.result(
+            :title      => title,
+            :navigation => nav,
+            :thumbnails => thumbnails.each_slice(5).to_a
+          )
+        end
       end
   end
 end
