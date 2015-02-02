dff366e242c8d2f3b9a78cb2b1f8fd552ac55499
Pouya Arvandian <pouya314@gmail.com>
2015-02-02 22:19:14 +0800

    update settings file.
    Add a few constants we will need later on.

--- lib/redbubble/settings.rb
+++ lib/redbubble/settings.rb
@@ -1,1 +1,8 @@
 # settings/configurations go here ...
+OUTPUT_TEMPLATE = "#{File.expand_path(File.dirname(__FILE__))}/templates/output-template.eruby"
+OUTPUT_PROTOCOL = "file://"
+
+INDEX = {
+  :file_name => "index.html",
+  :title     => "index"
+}
