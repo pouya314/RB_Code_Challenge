3796bc5dec9db84b005d853924ab6336e4379db4
Pouya Arvandian <pouya314@gmail.com>
2015-02-02 22:47:54 +0800

    update runner to include browser launch feature.
    After the html generation is done, use launchy gem API to launch the default system browser and navigate to the index page.

--- lib/redbubble/runner.rb
+++ lib/redbubble/runner.rb
@@ -1,6 +1,10 @@
 require_relative 'errors'
+require_relative 'settings'
 require_relative 'input_helper'
 require_relative 'input_processing'
+require_relative 'html_generation'
+require 'launchy'
+
 
 module Redbubble
   class Runner
@@ -9,14 +13,20 @@ module Redbubble
         # COMMAND LINE ARGS VALIDATION & PREP
         user_input = InputHelper.new(ARGV)
         input_file_path, output_dir_path = user_input.validate_and_prep!
-        
+
         # XML PROCESSING
         parser = GenericParser.new
         works = parser.parse(XmlParser.new(input_file_path))
 
         # HTML GENERATION
-        # generation = HtmlGeneration.new(works, output_dir_path)
-        # generation.go!
+        generation = HtmlGeneration.new(works, output_dir_path)
+        generation.go!
+
+        # Launch the browser (if possible)
+        uri = "#{OUTPUT_PROTOCOL}#{output_dir_path}#{INDEX[:file_name]}"
+        Launchy.open(uri) do |exception|
+          puts "Attempted to open #{uri} and failed because #{exception}"
+        end
       rescue NoArgumentsPassed, WrongNoOfArgs, InputFileDoesNotExist => e
         puts "Input arguments error: #{e}"
         puts "StackTrace: #{e.backtrace}"
