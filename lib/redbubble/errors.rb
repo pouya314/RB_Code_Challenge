287443f0c52ccd5329dc408f5d49a4b2572eb593
Pouya Arvandian <pouya314@gmail.com>
2015-02-02 13:51:08 +0800

    Give structure to program specific errors.
    define namespace for program specific exceptions. define input arguments errors in this commit. such exceptions are thrown by input helper class.

--- lib/redbubble/errors.rb
+++ lib/redbubble/errors.rb
@@ -1,6 +1,11 @@
-class RedBubbleException < Exception; end
+class RedBubbleError < Exception; end
 
-class WrongNoOfArgs < RedBubbleException; end
-class InputFileDoesNotExist < RedBubbleException; end
+  class InputArgumentsError < RedBubbleError; end
+    class NoArgumentsPassed < InputArgumentsError; end
+    class WrongNoOfArgs < InputArgumentsError; end
+    class InputFileDoesNotExist < InputArgumentsError; end
+
+
+# class InputFileDoesNotExist < RedBubbleException; end
 
 # ... etc
