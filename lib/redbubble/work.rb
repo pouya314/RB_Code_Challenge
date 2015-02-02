5f40ad91b3f6c8e0842ae033307fa72a4e437d59
Pouya Arvandian <pouya314@gmail.com>
2015-02-02 12:00:16 +0800

    Work data model class.
    some initial thoughts on how this model object might look like.

--- lib/redbubble/work.rb
+++ lib/redbubble/work.rb
@@ -1,1 +1,11 @@
-# the word data model class goes here...
+module Redbubble
+  class Work
+    attr_accessor :url, :make_name, :model_name
+    
+    def initialize(url, make_name, model_name)
+      @url        = url
+      @make_name  = make_name
+      @model_name = model_name
+    end
+  end
+end
