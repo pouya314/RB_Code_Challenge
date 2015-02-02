bb6d6d19d628f2545d9a0cd28eae3188f1034522
Pouya Arvandian <pouya314@gmail.com>
2015-02-02 13:55:24 +0800

    Update Runner.
    Include exception handling for errors that might be raised by input helper class.

--- lib/redbubble/runner.rb
+++ lib/redbubble/runner.rb
@@ -1,17 +1,30 @@
+require_relative 'errors'
+require_relative 'input_helper'
+
 module Redbubble
   class Runner
     def run
-      # COMMAND LINE ARGS VALIDATION & PREP
-      user_input = InputHelper.new(ARGV)
-      input_file_path, output_dir_path = user_input.validate_and_prep!
-      
-      # XML PROCESSING
-      parser = GenericParser.new
-      works = parser.parse(XMLParser.new(input_file_path))
+      begin
+        # COMMAND LINE ARGS VALIDATION & PREP
+        user_input = InputHelper.new(ARGV)
+        input_file_path, output_dir_path = user_input.validate_and_prep!
+        
+        # XML PROCESSING
+        # parser = GenericParser.new
+        # works = parser.parse(XMLParser.new(input_file_path))
 
-      # HTML GENERATION
-      generation = HtmlGeneration.new(works, output_dir_path)
-      generation.go!
+        # HTML GENERATION
+        # generation = HtmlGeneration.new(works, output_dir_path)
+        # generation.go!
+      rescue InputArgumentsError => e
+        puts "InputArgumentsError: #{e.message}"
+        puts "StackTrace: #{e.backtrace}"
+        exit
+      rescue Exception => e
+        puts "Error: #{e.message}"
+        puts "StackTrace: #{e.backtrace}"
+        exit
+      end
     end
   end
 end
