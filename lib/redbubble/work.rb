ff62a98ab9557dcb47ee666c842bfe73ad8c230d
Pouya Arvandian <pouya314@gmail.com>
2015-02-02 22:21:42 +0800

    Work object validity.
    Add ability to ask a work model object whether it's valid or not.

--- lib/redbubble/work.rb
+++ lib/redbubble/work.rb
@@ -1,11 +1,15 @@
 module Redbubble
   class Work
     attr_accessor :url, :make_name, :model_name
-    
+
     def initialize(url, make_name, model_name)
       @url        = url
       @make_name  = make_name
       @model_name = model_name
+    end
+
+    def invalid?
+      url == nil || make_name == nil || model_name == nil
     end
   end
 end
