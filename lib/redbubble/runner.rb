6f8fe37a72e7b90ac01d3cded1fc300e91c9fad9
Pouya Arvandian <pouya314@gmail.com>
2015-02-02 12:15:00 +0800

    Tidy up runner a little bit.
    Now the overall design is pretty much clear. on to implementing classes that runner uses.

--- lib/redbubble/runner.rb
+++ lib/redbubble/runner.rb
@@ -1,18 +1,15 @@
 module Redbubble
   class Runner
     def run
-      # 1) Send ARGV (Array) to input helper for:
-      # - Input Validation (correct number of arguments, existance of xml file, etc)
-      # - Folder structure creation if necessary
+      # COMMAND LINE ARGS VALIDATION & PREP
       user_input = InputHelper.new(ARGV)
-      input_file_path, output_dir_path = user_input.sanitize!
+      input_file_path, output_dir_path = user_input.validate_and_prep!
       
-      
-      # 2) all_works = [work, work, work, ...] = Send argv2 to InputProcessor for processing (be it XML, HTML, or ...)      
+      # XML PROCESSING
       parser = GenericParser.new
       works = parser.parse(XMLParser.new(input_file_path))
-      
-      # 3) tell HTMLProducer to produce the static html files..
+
+      # HTML GENERATION
       generation = HtmlGeneration.new(works, output_dir_path)
       generation.go!
     end
