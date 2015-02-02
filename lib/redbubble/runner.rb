f65423dcd83fd6e5ce55287bc53d7a39b421f6e7
Pouya Arvandian <pouya314@gmail.com>
2015-02-02 11:57:51 +0800

    Think about the overall flow of the application.
    - get rid of controller, as runner can own all the global business objects.
    - devise an overall flow/design

--- lib/redbubble/runner.rb
+++ lib/redbubble/runner.rb
@@ -1,8 +1,20 @@
-require_relative 'rbsystem_controller'
-
 module Redbubble
-  
-  # This class takes input from user, routes the input to controller, and returns the response eventually.
   class Runner
+    def run
+      # 1) Send ARGV (Array) to input helper for:
+      # - Input Validation (correct number of arguments, existance of xml file, etc)
+      # - Folder structure creation if necessary
+      user_input = InputHelper.new(ARGV)
+      input_file_path, output_dir_path = user_input.sanitize!
+      
+      
+      # 2) all_works = [work, work, work, ...] = Send argv2 to InputProcessor for processing (be it XML, HTML, or ...)      
+      parser = GenericParser.new
+      works = parser.parse(XMLParser.new(input_file_path))
+      
+      # 3) tell HTMLProducer to produce the static html files..
+      generation = HtmlGeneration.new(works, output_dir_path)
+      generation.go!
+    end
   end
 end
