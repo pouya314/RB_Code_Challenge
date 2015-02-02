8486e9ba0186a82c4d737ea03c1d125e7ddfac90
Pouya Arvandian <pouya314@gmail.com>
2015-02-02 12:16:50 +0800

    put some thought into what InputHelper is supposed to do.
    quickly scribble thoughts on validation and preparation procedures InputHelper needs to perform.

--- lib/redbubble/input_helper.rb
+++ lib/redbubble/input_helper.rb
@@ -1,5 +1,14 @@
 module Redbubble
   class InputHelper
-    # ...
+    def initialize(arguments)
+      @arguments = arguments
+    end
+    
+    def validate_and_prep!
+      # command line arguments are not empty?
+      # number of command line arguments is 2.
+      # does the input file exist?
+      # does the output directory exist? if not, maybe create it?
+    end
   end
 end
