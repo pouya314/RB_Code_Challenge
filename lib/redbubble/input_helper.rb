ae5b9a3aa20cb2abe92e63e29b0a17fd376b611d
Pouya Arvandian <pouya314@gmail.com>
2015-02-02 13:54:04 +0800

    Implement input helper.
    Perform the following validations/preparations on the command-line arguments passed to our program, ensure:
    -  arguments array is not empty.
    -  exactly 2 arguments are passed.
    -  input file specified actually exists.
    -  [prep] if output dir doesn't exist, create it.

--- lib/redbubble/input_helper.rb
+++ lib/redbubble/input_helper.rb
@@ -1,14 +1,28 @@
+require_relative 'errors'
+require 'fileutils'
+
 module Redbubble
   class InputHelper
-    def initialize(arguments)
-      @arguments = arguments
+    attr_accessor :cmd_arguments
+    
+    def initialize(cmd_arguments)
+      @cmd_arguments = cmd_arguments
     end
     
     def validate_and_prep!
-      # command line arguments are not empty?
-      # number of command line arguments is 2.
-      # does the input file exist?
-      # does the output directory exist? if not, maybe create it?
+      raise NoArgumentsPassed, "You need to pass in some arguments." if cmd_arguments.empty?
+      raise WrongNoOfArgs, "This program expects exactly 2 arguments." unless cmd_arguments.count == 2
+      
+      input_file_path, output_dir_path = cmd_arguments[0], cmd_arguments[1]
+      
+      raise InputFileDoesNotExist, "Input file you specified does NOT exist." unless File.file? input_file_path
+      
+      unless Dir.exist? output_dir_path
+        FileUtils::mkdir_p output_dir_path
+        puts "The output directory path you specified did not exist, but we created it for you."
+      end
+      
+      return input_file_path, output_dir_path
     end
   end
 end
