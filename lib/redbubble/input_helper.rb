af7b08dcf9a92b03dd8e7e868e8fabf2f1ca741b
Pouya Arvandian <pouya314@gmail.com>
2015-02-03 12:26:26 +0800

    Move validation from input helper to their respective classes.
    when you think about it, the validations/preps done in this class really should be done in "input processing" and "html generation" classes. If those modules are called internally, they need to guard against bad input.

--- lib/redbubble/input_helper.rb
+++ lib/redbubble/input_helper.rb
@@ -1,6 +1,5 @@
 require_relative 'errors'
 require_relative 'constants'
-require 'fileutils'
 
 
 module Redbubble
@@ -15,16 +14,14 @@ module Redbubble
       raise NoArgumentsPassed, ERRORS[:no_arguments_passed] if cmd_arguments.empty?
       raise WrongNoOfArgs, ERRORS[:wrong_number_of_arguments] unless cmd_arguments.count == 2
 
-      input_file_path, output_dir_path = cmd_arguments[0], cmd_arguments[1]
+      return cmd_arguments[0], cmd_arguments[1]
 
-      raise InputFileDoesNotExist, ERRORS[:input_file_does_not_exist] unless File.file? input_file_path
-
-      unless Dir.exist? output_dir_path
-        FileUtils::mkdir_p output_dir_path
-        puts ERRORS[:output_dir_did_not_exist_but_created]
-      end
-
-      return input_file_path, output_dir_path
+      # raise InputFileDoesNotExist, ERRORS[:input_file_does_not_exist] unless File.file? input_file_path
+      #
+      # unless Dir.exist? output_dir_path
+      #   FileUtils::mkdir_p output_dir_path
+      #   puts ERRORS[:output_dir_did_not_exist_but_created]
+      # end
     end
   end
 end
