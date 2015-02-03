fdad323b4bc533d975669db5f391146114d520bf
Pouya Arvandian <pouya314@gmail.com>
2015-02-03 15:45:57 +0800

    move preparation procedure to html generation class.
    this preparation (i.e. to make sure output directory exists before generating static files) needs to be done in html generation class.

--- lib/redbubble/input_helper.rb
+++ lib/redbubble/input_helper.rb
@@ -13,13 +13,7 @@ module Redbubble
     def validate_argv!
       raise NoArgumentsPassed, ERRORS[:no_arguments_passed] if cmd_arguments.empty?
       raise WrongNoOfArgs, ERRORS[:wrong_number_of_arguments] unless cmd_arguments.count == 2
-
       return cmd_arguments[0], cmd_arguments[1]
-
-      # unless Dir.exist? output_dir_path
-      #   FileUtils::mkdir_p output_dir_path
-      #   puts ERRORS[:output_dir_did_not_exist_but_created]
-      # end
     end
   end
 end
