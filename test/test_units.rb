cae77e9f43d43da386e0e93769d99d123d0e9b90
Pouya Arvandian <pouya314@gmail.com>
2015-02-04 00:44:34 +0800

    Rename unit test file.
    choose a more proper name for it.

--- test/test_units.rb
+++ test/test_units.rb
@@ -1,8 +1,7 @@
 require 'minitest/autorun'
 
+# unit tests
 require_relative 'test_work'
 require_relative 'test_input_helper'
 require_relative 'test_input_processing'
 require_relative 'test_html_generation'
-
-require_relative 'test_system'
