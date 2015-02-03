240ebaf35b3de57250791ac2adaa13841b1a25d9
Pouya Arvandian <pouya314@gmail.com>
2015-02-03 20:47:10 +0800

    Integrate Capybara.
    for end to end system testing. to be completed.

--- test/test_app.rb
+++ test/test_app.rb
@@ -4,3 +4,5 @@ require_relative 'test_work'
 require_relative 'test_input_helper'
 require_relative 'test_input_processing'
 require_relative 'test_html_generation'
+
+require_relative 'test_system'
